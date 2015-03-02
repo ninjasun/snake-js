44d250f1ed4a42615c575535e16b182699cbf258
ninjasun <davide.mezzetti@hotmail.it>
2015-03-02 19:14:43 +0100

    drawing on canvas

--- script.js
+++ script.js
@@ -4,18 +4,18 @@
 
 /* factory to create a cell prototype 10x10*/
 
-function cell(x, y, type) {
+function cell(x, y, type, color) {
     var cell = {
         x: x,//width cord
         y: y, //height cord
-        type: type
+        w : 10,
+        h : 10,
+        type: type,
+        color : color
     }
     return{
         get: function get() {
             return cell
-        },
-        getType: function getTipe() {
-            return this.type
         }
     }
 }
@@ -32,7 +32,7 @@ var snake = {
     },
     init: function init() {
         for (var i = 0; i < this.length; i++) {
-            this.struct[i] = cell(10, 10 + i, "snake").get()
+            this.struct[i] = cell(10, 10 + i, "snake", "green").get()
         }
         return this;
     }
@@ -46,7 +46,7 @@ var board = {
         for (var i = 0; i < this.height; i++) {
             this.struct[i] = []
             for (var j = 0; j < this.width; j++) {
-                this.struct[i][j] = cell(i, j, "empty").get()
+                this.struct[i][j] = cell(i, j, "empty", "blue").get()
             }
         }
         return board;
@@ -62,8 +62,8 @@ var food = {
         return this
     },
     init : function init(){
-        this.struct = cell(5,5,"food").get()
-        return this
+        this.struct[0] = cell(25,25,"food","red").get()
+        return food
     }
 }
 
@@ -71,6 +71,7 @@ var game = {
     board: board.init(),
     snake: snake.init(),
     food: food.init(),
+    zoomView : 10,
     get: function get() {
         return this
     },
@@ -78,11 +79,12 @@ var game = {
 // put snake and food into board matrix
         this.snake.struct.forEach(function (item) {
             //console.log("item is: ", item)
-            this.board.struct[item.x][item.y].type = "snake"
+            this.board.struct[item.x][item.y] = item
+            console.log("adding item: ",item)
         })
-        var food_struct = this.food.struct
-       // console.log("food is: ",food_struct)
-        this.board.struct[food_struct.x][food_struct.y].type = "food";
+        this.food.struct.forEach(function(item){
+            this.board.struct[item.x][item.y] = item;
+        })// note that food is one box
         return game;
     },
     start: function start() {
@@ -100,7 +102,18 @@ var game = {
     },
     draw : function draw(){
         //draw on canvas
+        var c = document.getElementById("canvas");
+        var ctx = c.getContext("2d");
+        this.board.struct.forEach(function(row){
+                //console.log(row)
+                row.forEach(function(item){
+                    //console.log("drawing a: ",item)
+                    ctx.fillStyle = item.color;
+                    ctx.fillRect(item.x * 10,item.y * 10,item.h,item.w);
+                })
+            }
+        )
     }
 }
 
-console.log(game.init().printBoard())
+
