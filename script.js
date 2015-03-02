22637331b57ac940081e4ba6caf5290839313fb0
ninjasun <davide.mezzetti@hotmail.it>
2015-03-02 12:15:43 +0100

    creating factory board

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 /**
  * Created by ilpirata on 02/03/2015.
  */
+/* factory to create a board w*h */
+function board(w,h){
+    var board = [];
+
+    return {
+        get: function get(){
+            return board;
+        },
+        print: function print(){
+            console.log("board is: ", board)
+        }
+    }
+}
